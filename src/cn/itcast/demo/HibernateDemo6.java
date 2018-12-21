34be085d811a0d240b383428639c4cb46cfab0c3
eric <1479124685@qq.com>
2018-12-21 23:03:48 +0800

    修改 HibernateDemo6.java 明确一对多关联中，保存数据的时候，如果避免产生额外的update 语句

--- src/cn/itcast/demo/HibernateDemo6.java
+++ src/cn/itcast/demo/HibernateDemo6.java
@@ -30,32 +30,49 @@ public class HibernateDemo6 {
 		Transaction transaction = session.beginTransaction();
 		
 		Customer c = new Customer();
-		c.setName("Jacky");
-		session.save(c);
+		c.setName("Rose1");
+		
 		
 		Order o1 = new Order();
-		o1.setPrice(10.52);
+		o1.setPrice(102.52);
 		
 		Order o2 = new Order();
-		o2.setPrice(100.52);
+		o2.setPrice(1002.52);
 		
 		Order o3 = new Order();
-		o3.setPrice(1000.52);
-		
-		session.save(o1);
-		session.save(o2);
-		session.save(o3);
+		o3.setPrice(10002.52);
 		
 		// 设置关联关系
 		// 客户关联订单
-		c.getOrderSet().add(o1);
-		c.getOrderSet().add(o2);
-		c.getOrderSet().add(o3);
+		// 注意：关联的时候不要双向关联，【最好是通过多方去关联一方】
+		// 因为外键列在多方,你通过多方关联一方的时候，其实就是明确外键值，插入的时候直接完事儿
+		// 反过来，如果你通过一方去关联多方的话，那么你插入一方时不能顺便插入另一张表的外键列的值, 你还得根据
+		// orderSet 集合里面的 order 对应的主键id 去多方表更新数据
+//		c.getOrderSet().add(o1);
+//		c.getOrderSet().add(o2);
+//		c.getOrderSet().add(o3);
 		
 		// 订单关联客户
 		o1.setCustomer(c);
 		o2.setCustomer(c);
 		o3.setCustomer(c);
+		
+		// 保存对象
+		// 保存对象的顺序也是有讲究的：
+		// 如果我们先保存一方，那么外键值也就是一方的主键值就明确了，这时候如果你又是通过多方去关联一方
+		// 那么在插入order 对象的时候，外键值就是明确的，不需要额外的更新语句
+		// 反过来，如果我们先保存多方，这时因为一方还没有保存，主键值还没有产生，所以多方表的外键值只能暂时先写 null
+		// 等一方插入数据后，主键值有了，我们再根据关联关系，去更新多方表的外键值
+		
+		// 【注意】 如果我们遵循这个规则的话,即： 使用多方去关联一方，然后保存对象的时候先保存一方再保存多方
+		// 那么本次的插入只会产生四个 Insert 语句，根本不会产生额外的 update 语句
+		session.save(c);
+		
+		session.save(o1);
+		session.save(o2);
+		session.save(o3);
+		
+		
 		
 		transaction.commit();
 		session.close();
